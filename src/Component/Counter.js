2d7713ec4dd85927e9e84f839397415b7c9d9c9c
sindhubedare <sindhubedare1990@gmail.com>
2020-08-13 14:04:40 -0500

    working code

---
+++ src/Component/Counter.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import '../App.css';
+
+class Counter extends React.Component {
+  state = { count: 0 }
+
+  increment = () => {
+      this.props.dispatch({type: "INCREMENT"})
+    // this.setState({
+    //   count: this.state.count + 1
+    // });
+  }
+
+  decrement = () => {
+    this.props.dispatch({type: "DECREMENT"})
+    // this.setState({
+    //   count: this.state.count - 1
+    // });
+  }
+ 
+
+  render() {
+    return (
+      <div className="counter">
+        <h2>Counter</h2>
+        <div className="button">
+          <button  onClick={this.decrement}>Decrement</button>
+          <span>{this.props.count}</span>
+          <button onClick={this.increment}>Increment</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({
+    
+        count: state.count
+})
+
+export default connect (mapStateToProps) (Counter);
---
+++ src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Counter from './Component/Counter';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
+const initialState={
+  count: 0
+};
+function reducer(state = initialState, action){
+
+  switch(action.type){
+    case "INCREMENT" :
+      return {
+        count: state.count + 1
+      };
+      case "DECREMENT" :
+        return {
+          count: state.count - 1
+        };
+      default:
+        return state;
+  }
+
+}
+const store = createStore(reducer);
+// store.dispatch({ type: "INCREMENT"})
+// store.dispatch({ type: "DECREMENT"})
+const App = () => (
+
+    <Provider store= {store}>
+    <Counter />
+    </Provider>
+  
+);
+
+render(<App />, document.getElementById('root'));
